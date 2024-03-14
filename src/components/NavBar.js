87779a2990b14bcc0a9513c2503ee33d0ec738b3
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-03-14 12:06:22 +0000

    Install router library and put in basic navigation between pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import logo from '../assets/logo.png'
-import styles from '../styles/NavBar.module.css'
+import React, { Component } from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 export class NavBar extends Component {
 
@@ -17,24 +20,36 @@ export class NavBar extends Component {
     return (
       <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-          <Navbar.Brand>
-            <img src={logo} alt='logo' height='65'/>
-          </Navbar.Brand>
-          <Nav.Link className='mr-auto'>
+          <NavLink to="/">
+            <Navbar.Brand>
+              <img src={logo} alt='logo' height='65'/>
+            </Navbar.Brand>
+          </NavLink>
+          <NavLink className='mr-auto' to="/">
             STRONGEST<br/>AVENGER   
-          </Nav.Link>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
-              <Nav.Link>
+              <NavLink
+                exact
+                to="/"
+                activeClassName={styles.Active}
+              >
                 <i className="fas fa-home"></i>Home
-              </Nav.Link>
-              <Nav.Link>
+              </NavLink>
+              <NavLink
+                to="signin"
+                activeClassName={styles.Active}
+              >
                 <i className="fas fa-sign-in-alt"></i>Sign in
-              </Nav.Link>
-              <Nav.Link>
+              </NavLink>
+              <NavLink
+                to="signup"
+                activeClassName={styles.Active}
+              >
                 <i className="fas fa-user-plus"></i>Sign up
-              </Nav.Link>
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
