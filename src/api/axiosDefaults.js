7974b7ff08871a0e0257e3a4186e01f50b0a5578
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-03-15 12:28:25 +0000

    Add ability for user to submit signup form and create account

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.basURL = 'https://strongest-avenger-drf-api-c57eb1c3e676.herokuapp.com/'
+axios.defaults.baseURL = 'https://strongest-avenger-drf-api-c57eb1c3e676.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,18 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignUpForm.module.css";
-import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 import logo from '../../assets/logo.png';
-
-
+import axios from "axios";
+import { useState } from "react";
 
 const SignUpForm = () => {
+
+  {/* Use state hook for Signup data */}
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  {/* Use state hook for errors */}
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  {/* Function so input fields display whats being typed */}
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  {/* Function to submit sign up form */}
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      console.error("Error submitting form:", err);
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Container className={styles.Content}>
       <Row className={`${styles.Row} justify-content-center`}>
@@ -32,7 +66,7 @@ const SignUpForm = () => {
       <Row className="justify-content-center text-center p-4">
         <Col md={{ span: 6 }}>
           <Container>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group controlId="username">
                 <Form.Label className="d-none">username</Form.Label>
                 <Form.Control
@@ -40,8 +74,15 @@ const SignUpForm = () => {
                   type="text"
                   placeholder="Username"
                   name="username"
+                  value={username}
+                  onChange={handleChange}
                 />
               </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
               <Form.Group controlId="password1">
                 <Form.Label className="d-none">Password</Form.Label>
                 <Form.Control
@@ -49,8 +90,15 @@ const SignUpForm = () => {
                   type="password"
                   placeholder="Password"
                   name="password1"
+                  value={password1}
+                  onChange={handleChange}
                 />
               </Form.Group>
+              {errors.password1?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
               <Form.Group controlId="password2">
                 <Form.Label className="d-none">Password</Form.Label>
                 <Form.Control
@@ -58,11 +106,23 @@ const SignUpForm = () => {
                   type="password"
                   placeholder="Confirm password"
                   name="password2"
+                  value={password2}
+                  onChange={handleChange}
                 />
               </Form.Group>
+              {errors.password2?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
               <Button className={btnStyles.Button} type="submit">
                 Sign up
               </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant="warning" className="mt-3">
+                  {message}
+                </Alert>
+              ))}
             </Form>
           </Container>
         </Col>
