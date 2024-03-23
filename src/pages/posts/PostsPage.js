41c319b9b8c7b8ed1d4758786dd23421cab9dc50
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-03-23 12:09:47 +0000

    Add search bar

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import styles from "../../styles/PostsPage.module.css";
 import appStyles from "../../App.module.css";
 import NoResultsImg from "../../assets/no-results.png";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -13,11 +15,12 @@ const PostsPage = ({ message, filter = "" }) => {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -26,22 +29,31 @@ const PostsPage = ({ message, filter = "" }) => {
     };
 
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
 
   let postsContent;
+  // Whilst loading, screen will display spinner
   if (!hasLoaded) {
     postsContent = (
       <Container className={appStyles.Content}>
         <Asset spinner />
       </Container>
     );
+    // If there are no results for the posts
   } else if (posts.results.length === 0) {
     postsContent = (
       <Container className={appStyles.Content}>
         <Asset src={NoResultsImg} message={message} />
       </Container>
     );
+    // Display the list of posts
   } else {
     postsContent = posts.results.map((post) => (
       <Post key={post.id} {...post} setPosts={setPosts} />
@@ -57,6 +69,18 @@ const PostsPage = ({ message, filter = "" }) => {
         <Col lg={{ span: 6, offset: 1 }}>
           <p>Popular for mobiles</p>
           <p>Search by mobiles</p>
+          <i className={`fas fa-search ${styles.SearchIcon}`} />
+          <Form
+            className={styles.SearchBar}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <Form.Control
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              placeholder="Search posts"
+            />
+          </Form>
           {postsContent}
         </Col>
         <Col lg={{ span: 2, offset: 1 }} className="d-none d-lg-block">
